2e0f4e738363e96a2b0dc211947266d19266270c
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-10-02 21:47:38 +0200

    Copy of FSharpX code in order to show c# compatibility

---
+++ FSharp.Compatibility.CSharp.Tests/ArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+using FSharp.Compatibility.CSharp;
+
+namespace FSharpx.CSharpTests {
+    [TestFixture]
+    public class ArrayTests {
+        [Test]
+        public void Does_not_conflict_with_System_Array() {
+            var a = new[] {1,2,3};
+            Array.Clear(a, 0, 0);
+        }
+    }
+}
